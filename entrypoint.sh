71c5245150941149f73f562869f6937462495ae5
Daniel Kim <dgkim@ucdavis.edu>
2020-12-30 23:41:53 -0500

    initial-commit

---
+++ entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+FOLDER=$1
+GITHUB_USERNAME=$2
+REPO_NAME="${3:-name}"
+BRANCH_NAME="${4:-main}"
+BASE=$(pwd)
+
+git config --global user.email "johno-actions-push-subdirectories@example.org"
+git config --global user.name "$GITHUB_USERNAME"
+
+echo "Cloning folders in $FOLDER and pushing to $GITHUB_USERNAME"
+
+# sync to read-only clones
+for folder in $FOLDER/*; do
+  [ -d "$folder" ] || continue # only directories
+  cd $BASE
+
+  echo "$folder"
+
+  echo "  Name: $REPO_NAME"
+  IS_WORKSPACE=$(cat $folder/package.json | jq -r '.workspaces')
+  CLONE_DIR="__${REPO_NAME}__clone__"
+  echo "  Clone dir: $CLONE_DIR"
+
+  # clone, delete files in the clone, and copy (new) files over
+  # this handles file deletions, additions, and changes seamlessly
+  git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$REPO_NAME.git $CLONE_DIR &> /dev/null
+  cd $CLONE_DIR
+  find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
+  cp -r $BASE/$folder/. .
+
+  # generate a new yarn.lock file based on package-lock.json unless you're in a workspace
+  if [ "$IS_WORKSPACE" = null ]; then
+    echo "  Regenerating yarn.lock"
+    rm -rf yarn.lock
+    yarn
+  fi
+
+  # Commit if there is anything to
+  if [ -n "$(git status --porcelain)" ]; then
+    echo  "  Committing $REPO_NAME to $GITHUB_REPOSITORY"
+    git add .
+    git commit --message "Update $REPO_NAME from $GITHUB_REPOSITORY"
+    git push origin $BRANCH_NAME
+    echo  "  Completed $REPO_NAME"
+  else
+    echo "  No changes, skipping $REPO_NAME"
+  fi
+
+  cd $BASE
+done
