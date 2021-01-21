767c40375111e48e7be7b32542af19ca2cd8573f
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-01-20 19:10:27 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ for folder in $FOLDER/*; do
 
   # clone, delete files in the clone, and copy (new) files over
   # this handles file deletions, additions, and changes seamlessly
-  git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$REPO_NAME.git $CLONE_DIR # &> /dev/null
+  git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$REPO_NAME.git $CLONE_DIR &> /dev/null
   cd $CLONE_DIR
   find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
   mkdir -p ./$foldername
@@ -46,6 +46,7 @@ for folder in $FOLDER/*; do
   else
     echo "  No changes, skipping $BASE/$folder/"
   fi
-
+  
+  rmdir -r $CLONE_DIR
   cd $BASE
 done
