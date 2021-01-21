860d7b09bb920c432aea8576385a49777f52d39a
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-01-20 20:37:19 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,7 @@ for folder in $FOLDER/*; do
   # this handles file deletions, additions, and changes seamlessly
   git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$REPO_NAME.git $CLONE_DIR &> /dev/null
   cd $CLONE_DIR
+  ls
   find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
   mkdir -p ./$foldername
   cp -r $BASE/$folder/. ./$foldername
